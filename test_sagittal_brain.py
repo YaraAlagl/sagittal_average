6dc6be13ae370274c654a08d7796481bb7bac46b
YaraAlagl <alagl.yara@gmail.com>
2025-11-10 10:48:01 +0000

    Edited input and output code

--- test_sagittal_brain.py
+++ test_sagittal_brain.py
@@ -1,5 +1,12 @@
 import numpy as np
 
+# Create input
 data_input = np.zeros((20, 20))
 data_input[-1, :] = 1
 np.savetxt("brain_sample.csv", data_input, fmt='%d', delimiter=',')
+
+# Create expected output
+# (for example: if sagittal_average mirrors left and right halves)
+expected_output = np.zeros((20, 20))
+expected_output[-1, :] = 1  # same as input, since it's symmetric
+np.savetxt("brain_expected.csv", expected_output, fmt='%d', delimiter=',')
