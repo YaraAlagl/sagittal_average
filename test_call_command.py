b7886375fc2ad28467be0d72405b6ece92f27312
YaraAlagl <alagl.yara@gmail.com>
2025-11-10 11:13:15 +0000

    Used subprocess.run to call charlene's program (Issue 28)

--- test_call_command.py
+++ test_call_command.py
@@ -1,3 +1,10 @@
 import subprocess
 
-subprocess.run(["ls", "-aF"])
+#subprocess.run(["ls", "-aF"])
+import subprocess
+
+subprocess.run([
+    "python", "sagittal_brain.py",
+    "brain_sample.csv",
+    "--file_output", "brain_average.csv"
+], check=True)
